0f29a1eb5d2b1a478a1a65f858840dc60105e3e9
= <=>
2023-04-08 18:44:58 +0100

    modified services backend

--- frontend/components/services/backend.js
+++ frontend/components/services/backend.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { BACKEND_BASE_URL } from "../../utils/api";
 
-const key = "backend:api:token";
 
 function getTokenFromPersistedStorage() {
   if (typeof window !== "undefined") return window.localStorage.getItem(key);
@@ -12,21 +11,6 @@ export class Backend {
 
   #http = axios;
 
-  
-  // .create({
-  //   baseURL: BACKEND_BASE_URL,
-    // headers: {
-    //   Authorization: this.#token ? `Bearer ${this.#token}` : undefined,
-    // },
-  // });
-
-  // set token(value) {
-  //   localStorage.setItem(key, value);
-  //   this.#token = value;
-  //   this.#http.defaults.headers["Authorization"] = value
-  //     ? `Bearer ${this.#token}`
-  //     : undefined;
-  // }
 
   // async login(form) {
   //   const { data } = await this.#http.post("login", form);
@@ -72,72 +56,6 @@ export class Backend {
     return data;
   }
 
-
-
-
-
-
-
-  
-  async storeAgency(form) {
-    const { data } = await this.#http.post("agency/store", form);
-
-    return data;
-  }
-
-  async searchAgency(query) {
-    const { data } = await this.#http.get(`agency/search?query=${query}`);
-    return data;
-  }
-
-  async storeInvoice(form) {
-    const { data } = await this.#http.post("invoice/store", form);
-    return data;
-  }
-
-  
-  async getInvoice(id) {
-    const { data } = await this.#http.get(`invoice/${id}`);
-    return data;
-  }
-
-  async listInvoices(query = {}) {
-    const queryParams = Object.keys(query)
-      .map((key) => `${key}=${query[key]}`)
-      .join("&");
-
-    const { data } = await this.#http.get(`invoice/all?${queryParams}`);
-    return data;
-  }
-
-  async listActiveInvoices(query = {}) {
-    const queryParams = Object.keys(query)
-      .map((key) => `${key}=${query[key]}`)
-      .join("&");
-
-    const { data } = await this.#http.get(`invoice/active/all?${queryParams}`);
-    return data;
-  }
-
-  async getActiveInvoice(id) {
-    const { data } = await this.#http.get(`invoice/${id}/active`);
-    return data;
-  }
-
-  async lenderInvoiceStats() {
-    const { data } = await this.#http.get(`lender/contribution/stats`);
-    return data;
-  }
-
-  async listLenderContribution() {
-    const { data } = await this.#http.get(`lender/contribution/list`);
-    return data;
-  }
-
-  async adminInvoiceStats() {
-    const { data } = await this.#http.get("/invoice/stats");
-    return data;
-  }
 }
 
 const backend = new Backend();
