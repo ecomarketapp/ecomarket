723870a23344cbb448a1c752af189cb3d769b998
ChainBuilderPO <129603793+ChainBuilderPO@users.noreply.github.com>
2023-04-15 01:07:54 +0400

    change cta on create offer page

--- frontend/pages/company/create-offer.js
+++ frontend/pages/company/create-offer.js
@@ -336,7 +336,7 @@ const profile = () => {
                         type="submit"
                         className="inline-block px-7 py-3 text-white font-medium text-sm leading-snug uppercase rounded-full shadow-md bg-[#DD7D37] hover:shadow-lg  focus:shadow-lg focus:outline-none focus:ring-0  active:shadow-lg transition duration-150 ease-in-out w-full"
                       >
-                        {loading === true ? <Loader /> : 'Save Profile'}
+                        {loading === true ? <Loader /> : 'Create Offer'}
                       </button>
                     </form>
                   </div>
