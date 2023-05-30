66caf2c4b1a827116a41d7ee5a9f0519253389e8
ChainBuilderPO <habibidev@proton.me>
2023-05-30 15:00:17 +0400

    get collector deliveries

--- backend/app/routes/collector.routes.js
+++ backend/app/routes/collector.routes.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
 
 const Collector = require('../controllers/collector.controller');
+const Delivery = require('../controllers/delivery.controller');
 
-// Create a new Company
 router.get('/', Collector.getCollectors);
 router.post('/', Collector.createCollector);
 router.post('/:id/save', Collector.saveProfile);
 router.post('/auth/register', Collector.register);
 router.post('/auth/login', Collector.login);
 router.get('/:id', Collector.getOneCollector);
+router.get('/:id/deliveries', Delivery.getCollectorDeliveries);
 module.exports = router;
